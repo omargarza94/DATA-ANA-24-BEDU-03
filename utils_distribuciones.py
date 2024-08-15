efe9d2813db4f14bbe504b80b9a0bf6c572d6e28
Omar Garza <garza.omar.lbg@gmail.come>
2024-08-14 21:55:05 -0600

    ejemplos de plots

---
+++ utils_distribuciones.py
@@ -0,0 +1,25 @@
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from scipy.stats import skew, kurtosis
+
+'''
+Documentacion de una funcion o clase
+'''
+def generar_distribucion(data, titulo):
+    asimetria = skew(data)
+    curtosis = kurtosis(data)
+
+    print(f'{titulo}: Asimetria: {asimetria:.2f}, Curtosis: {curtosis:.2f}')
+
+    # Histograma
+    sns.histplot(data, kde=False)
+
+    plt.title(titulo)
+    plt.xlabel('Valor')
+    plt.ylabel('Frecuencia')
+    plt.grid(True)
+    plt.show()
+
+    print ("todo bien")
+    
